00dbce2cf5d51054f51121e6850f4e387cd0b51e
Vipin Singh <vipin.singh@giksindia.com>
2024-06-14 21:37:49 +0530

    constructor

---
+++ modules/com.practice.java/src/main/java/JavaPractice/constants/JavaPracticePortletKeys.java
@@ -0,0 +1,11 @@
+package JavaPractice.constants;
+
+/**
+ * @author hp
+ */
+public class JavaPracticePortletKeys {
+
+	public static final String JAVAPRACTICE =
+		"JavaPractice_JavaPracticePortlet";
+
+}
---
+++ modules/com.practice.java/src/main/java/JavaPractice/portlet/JavaPracticePortlet.java
@@ -0,0 +1,30 @@
+package JavaPractice.portlet;
+
+import JavaPractice.constants.JavaPracticePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author hp
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=JavaPractice",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + JavaPracticePortletKeys.JAVAPRACTICE,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class JavaPracticePortlet extends MVCPortlet {
+}
