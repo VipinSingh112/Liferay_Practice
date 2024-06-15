5f169b09ed69ef0f6e0d7988bb88290f68fe32dc
Vipin Singh <vipin.singh@giksindia.com>
2024-06-15 13:21:19 +0530

    constructor this keyword access modifier

---
+++ modules/com.practice.java/src/main/java/Constructor/ConstructorCallThroughThisKeyWord.java
@@ -0,0 +1,32 @@
+package Constructor;
+
+/*
+ * We can call constructor inside constructor  by the use of this keyword
+ * 
+ * 
+ * */
+
+public class ConstructorCallThroughThisKeyWord {
+
+	public ConstructorCallThroughThisKeyWord() {
+		this("vipin", "29", "Brahmpuri",976037099);
+	}
+
+	public ConstructorCallThroughThisKeyWord(String name, String age, String address) {
+		System.out.println("Name---------" + name);
+		System.out.println("Age---------" + age);
+		System.out.println("Address---------" + address);
+	}
+
+	protected ConstructorCallThroughThisKeyWord(String name, String age, String address, int mobNUmber) {
+		System.out.println("Name---------" + name);
+		System.out.println("Age---------" + age);
+		System.out.println("Address---------" + address);
+		System.out.println("Address---------" + mobNUmber);
+	}
+
+	public static void main(String[] args) {
+		ConstructorCallThroughThisKeyWord callThroughThisKeyWord = new ConstructorCallThroughThisKeyWord();
+	}
+
+}
